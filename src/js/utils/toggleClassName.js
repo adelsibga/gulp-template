4325dabf09a08f264652265a1180816710f3cbe0
Adel Sibgatullin <adelsibga@gmail.com>
2023-09-20 20:19:13 +0300

    docs add new func

---
+++ src/js/utils/toggleClassName.js
@@ -0,0 +1,11 @@
+const toggleClassName = (el, className) => {
+  if (el.classList.contains(className)) {
+    el.classList.remove(className)
+  } else {
+    el.classList.add(className)
+  }
+}
+
+export {
+  toggleClassName
+}
