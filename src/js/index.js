b97a898a2d64fb6c9f3cc30627abfe6d6e15e505
Adel Sibgatullin <adelsibga@gmail.com>
2023-11-05 00:26:59 +0300

    docs Добавил waterfall scroller для текста и картинок(хотя я думаю туда можно засунуть еще что-то интересненькое)

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 import { checkAccount } from './ispringUI/components/headers/top_menu_common.js'
 import { TopMobileMenu } from './ispringUI/components/headers/TopMobileMenu.js'
 import { ArrowNavigationMenuNew } from './ispringUI/components/headers/product/mobile.js'
-import { scrollerItemAnimation } from './components/scroller/scrollerItem.js'
 
 import {
     _mainMenuHandler,
@@ -26,7 +25,3 @@ document.addEventListener('DOMContentLoaded', () => {
     setVisibleDropDown()
     scrollingMenu()
 })
-
-if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-  scrollerItemAnimation()
-}
