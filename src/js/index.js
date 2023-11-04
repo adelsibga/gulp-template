81cd9d311837c6fe56e345c8b456d729c5c51566
Adel Sibgatullin <adelsibga@gmail.com>
2023-11-04 22:18:01 +0300

    docs Добавил scrollerItem для скроллера лого или другого контента

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import { checkAccount } from './ispringUI/components/headers/top_menu_common.js'
 import { TopMobileMenu } from './ispringUI/components/headers/TopMobileMenu.js'
 import { ArrowNavigationMenuNew } from './ispringUI/components/headers/product/mobile.js'
+import { scrollerItemAnimation } from './components/scroller/scrollerItem.js'
 
 import {
     _mainMenuHandler,
@@ -20,10 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
     _mainMenuHandler(e, n)
     selectInitNavigationMenu('js-not-logged', n, e)
     ShowSearchField()
-    // eslint-disable-next-line no-new
     new TopMobileMenu()
-    // eslint-disable-next-line no-new
     new ArrowNavigationMenuNew()
     setVisibleDropDown()
     scrollingMenu()
 })
+
+if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+  scrollerItemAnimation()
+}
