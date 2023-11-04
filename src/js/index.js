5638248b64a6874c92a56a206c1ab5570511d31f
Adel Sibgatullin <adelsibga@gmail.com>
2023-11-05 01:38:46 +0300

    docs Немного побаловался со scroller`ом - добавил 3 scroller`а и засунул в них логотипы и ссылки Добавил еще какую-то странную но прикольную штуку - с 0 до числа происходит набор по 100 или 1000

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import { checkAccount } from './ispringUI/components/headers/top_menu_common.js'
 import { TopMobileMenu } from './ispringUI/components/headers/TopMobileMenu.js'
 import { ArrowNavigationMenuNew } from './ispringUI/components/headers/product/mobile.js'
+import { scrollerItemAnimation } from './components/scroller/scrollerItem.js'
 
 import {
     _mainMenuHandler,
@@ -24,4 +25,32 @@ document.addEventListener('DOMContentLoaded', () => {
     new ArrowNavigationMenuNew()
     setVisibleDropDown()
     scrollingMenu()
+})
+
+if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+  scrollerItemAnimation()
+}
+
+function ClientsCounter(e) {
+  let t, n, r, o = parseInt(e.textContent.replace(/\D/g, ''))
+  e.classList.contains('active') && (t = 0,
+    n = Math.abs(Math.floor(o / 1e3)),
+    r = setInterval(function () {
+      t += n,
+        e.innerHTML = Math.ceil(t).toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ','),
+      t === o && (e.classList.remove('active'),
+        clearInterval(r))
+    }))
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let n = document.querySelector('.js-number-of-clients')
+    , r = new IntersectionObserver(function (e, t) {
+      e.forEach(function (e) {
+        e.isIntersecting && (ClientsCounter(n),
+          r.unobserve(e.target))
+      })
+    }
+  )
+  r.observe(n)
 })
