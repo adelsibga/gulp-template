a2c2cfbfd3a434249dc4327a5faead4b56876629
Adel Sibgatullin <adelsibga@gmail.com>
2024-04-11 13:35:22 +0300

    add simple dimple alg

---
+++ src/js/algorithms/quickSort.js
@@ -0,0 +1,23 @@
+const QuickSort = (List) => {
+  if (List.length <= 1) {
+    return List
+  }
+
+  const pivot = List[List.length - 1]
+  const leftList = []
+  const rightList = []
+
+  for (let i = 0; i < List.length - 1; i++) {
+    if (List[i] < pivot) {
+      leftList.push(List[i])
+    } else {
+      rightList.push(List[i])
+    }
+  }
+
+  return [...QuickSort(leftList), pivot, ...QuickSort(rightList)]
+}
+
+export {
+  QuickSort
+}
